1ad39d54ce841ec57389a036a241c2185eb3e277
Siew Wing Fei <siewwingfei@hotmail.com>
2019-07-13 18:03:36 +0800

    k

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,25 +5,35 @@ class TodoList extends Component {
 
   render() {
     const { todos, toggleComplete, deleteTodo, editTodo, updateTodo, cancelEditTodo } = this.props;
-    return (
-      <ul className="list-group">
-        {
-          todos.map(todo => (
-            <Todo key={todo.id} 
-              text={todo.text}
-              editText={todo.editText}
-              isEdit={todo.isEdit}
-              completed={todo.completed}
-              toggleComplete={() => toggleComplete(todo.id)}
-              deleteTodo={() => deleteTodo(todo.id)}
-              editTodo={() => editTodo(todo.id)}
-              updateTodo={(text) => updateTodo(todo.id, text)}
-              cancelEditTodo={() => cancelEditTodo(todo.id)}
-            />
-          ))
-        }
-      </ul>
-    )
+    if (!todos.length) {
+      return (
+        <div className="alert alert-info">
+          <div className="text-center">Your TO-DO is Empty</div>
+        </div>
+      )
+    }
+
+    else {
+      return (
+        <ul className="list-group">
+          {
+            todos.map(todo => (
+              <Todo key={todo.id}
+                text={todo.text}
+                editText={todo.editText}
+                isEdit={todo.isEdit}
+                completed={todo.completed}
+                toggleComplete={() => toggleComplete(todo.id)}
+                deleteTodo={() => deleteTodo(todo.id)}
+                editTodo={() => editTodo(todo.id)}
+                updateTodo={(text) => updateTodo(todo.id, text)}
+                cancelEditTodo={() => cancelEditTodo(todo.id)}
+              />
+            ))
+          }
+        </ul>
+      )
+    }
   }
 }
 
