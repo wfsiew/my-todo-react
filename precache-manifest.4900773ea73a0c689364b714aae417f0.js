059f0cec9cb1111e8d735a4b9bfe29137aaf707d
Siew Wing Fei <siewwingfei@hotmail.com>
2019-07-13 15:53:35 +0800

    Updates

---
+++ precache-manifest.4900773ea73a0c689364b714aae417f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522bf09a8fa7e050038cb773c69560e3",
+    "url": "/my-todo-react/index.html"
+  },
+  {
+    "revision": "a6714b07de4d13cf91d6",
+    "url": "/my-todo-react/static/css/main.48e03983.chunk.css"
+  },
+  {
+    "revision": "507666e33ba697746880",
+    "url": "/my-todo-react/static/js/2.bae1ca0c.chunk.js"
+  },
+  {
+    "revision": "a6714b07de4d13cf91d6",
+    "url": "/my-todo-react/static/js/main.e8cb29c7.chunk.js"
+  },
+  {
+    "revision": "f2c7bcc5c0b4cc8350b9",
+    "url": "/my-todo-react/static/js/runtime~main.35a35375.js"
+  }
+]);
